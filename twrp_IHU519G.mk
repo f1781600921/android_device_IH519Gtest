90c29f18628445485197587af4321cb5e3722d83
f1781600921 <1781600921@qq.com>
2023-12-02 14:35:34 +0800

    update

---
+++ twrp_IHU519G.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from IHU519G device
+$(call inherit-product, device/ecarx/IHU519G/device.mk)
+
+PRODUCT_DEVICE := IHU519G
+PRODUCT_NAME := twrp_IHU519G
+PRODUCT_BRAND := ecarx
+PRODUCT_MODEL := FS11A1
+PRODUCT_MANUFACTURER := ecarx
